f3af01353ecc5d8d362b939feb214b253752b89e
Marvin Deutz <marvin.deutz@yahoo.com>
2025-08-30 17:08:23 +0200

    Add prettier for astro and tailwind class formatting

--- hub/astro.config.mjs
+++ hub/astro.config.mjs
@@ -2,9 +2,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare({
+    imageService: "compile"
+  })
 });
