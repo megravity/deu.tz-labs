0ae78e02ef6dcae56633a8bd0a41a2863ef89488
Marvin Deutz <marvin.deutz@yahoo.com>
2025-09-01 00:00:55 +0200

    Remove background gradient and add gsap + test animation

--- hub/src/components/react/ExperimentBox.tsx
+++ hub/src/components/react/ExperimentBox.tsx
@@ -1,17 +1,20 @@
+import { forwardRef } from "react";
 import type { CardData } from "../../types/types";
 
 export interface Props {
   cardData: CardData;
 }
 
-export default function ExperimentBox(props: Props) {
-  const { cardData } = props;
+const ExperimentBox = forwardRef<HTMLDivElement, Props>(({ cardData }, ref) => {
   return (
     <div
+      ref={ref} // This is where the ref goes
       id={cardData.id}
-      className="card-3d-shadow text-shadow-[1px 1px black, -1px -1px black] relative flex aspect-square h-36 translate-3d rotate-x-10 rotate-y-7 rotate-z-5 transform-gpu flex-col justify-between rounded-md px-3 py-8 backdrop-blur-3xl transform-3d"
+      className="card-3d-shadow relative flex aspect-square h-36 translate-3d rotate-x-[10deg] rotate-y-[7deg] rotate-z-[5deg] transform-gpu flex-col justify-between rounded-md px-3 py-8 backdrop-blur-[500px] text-shadow-[1px_1px_black,_-1px_-1px_black]"
     >
       <h3 className="text-xl">{cardData.title}</h3>
     </div>
   );
-}
+});
+
+export default ExperimentBox;
--- hub/src/components/react/Experiments.tsx
+++ hub/src/components/react/Experiments.tsx
@@ -1,12 +1,42 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
 import { experiments } from "../../../public/mock-manifest.json";
 import type { CardData } from "../../types/types";
 import ExperimentBox from "./ExperimentBox";
 
+gsap.registerPlugin(useGSAP);
+
 export default function Experiments() {
+  const cardRefs = useRef<(HTMLDivElement | null)[]>([]); // Updated to allow null in array
+  useGSAP(
+    () => {
+      for (const ref of cardRefs.current) {
+        const yPos = Math.min(200, Math.random() * 800);
+        gsap.set(ref, { y: yPos });
+      }
+      // GSAP can access all cards
+      const cards = cardRefs.current.filter((ref) => {
+        ref !== null;
+        return ref;
+      });
+      gsap.fromTo(cards, { translateX: 0 }, { translateX: 360, stagger: 0.1 });
+    },
+    { scope: cardRefs }
+  );
+
   return (
     <div className="mt-14 flex justify-center gap-8 perspective-midrange transform-3d">
-      {(experiments as CardData[]).map((exp: CardData) => {
-        return <ExperimentBox key={exp.id} cardData={exp} />;
+      {(experiments as CardData[]).map((exp: CardData, index: number) => {
+        return (
+          <ExperimentBox
+            key={exp.id}
+            cardData={exp}
+            ref={(el) => {
+              cardRefs.current[index] = el; // This returns void
+            }}
+          />
+        );
       })}
     </div>
   );
